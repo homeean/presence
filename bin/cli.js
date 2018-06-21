7f6a58c6b35e5bb12ee6e478a68c2fd00007f611
stfnhmplr <stfnhmplr@gmail.com>
2018-06-21 20:36:32 +0200

    added cli

---
+++ bin/cli.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+process.title = 'homeean-presence';
+var path = require('path');
+var fs = require('fs');
+require(path.join(path.dirname(fs.realpathSync(__filename)), '../') + '/index');
